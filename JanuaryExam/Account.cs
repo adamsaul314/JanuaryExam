284a9d521b0a9f40c1d30479d79767ff86c6a1ff
S00197531 <S00197531@WIN-GVP95UGT0JU>
2021-01-11 09:49:55 +0000

    created abstract class acount and added properties

---
+++ JanuaryExam/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanuaryExam
+{
+    public abstract class Account
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public double Balance { get; set; }
+
+        public DateTime IntrestDate { get; set; }
+
+
+        public abstract double CalculateIntrest(); //Abstract method
+    }
+}
